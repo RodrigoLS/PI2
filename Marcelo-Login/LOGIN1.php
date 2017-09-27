1a0ffca70b0b9278b1218598f6b682e7f93a9c74
MarceloBH <marceloborellihegedus@gmail.com>
2017-09-27 00:55:58 -0300

    Login-Marcelo
    Html e Css página login loja Hippo.

---
+++ Marcelo-Login/LOGIN1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>ADO PHP</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div class="main grey lighten-2 center-align">		
+			<form  method="post" action="LOGIN2.php">
+			  <img class="circle" src="img/LogoHippo.jpg" alt="Logo Loja" width="120px" height="120px"><br>	
+			  <label> Usuário: </label>
+			  <br>
+			  <input type="text" name="user">
+			  <br>
+			  <label>Senha:</label>
+			  <input type="text" name="pass" >
+			  <br>
+			  <input class="waves-effect waves-light btn-large center-align red"type="submit" value="Submit">
+			</form> 
+	</div>
+</body>
+</html>
