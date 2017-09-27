8312a7f57b0fc3d104720922e5a7c879e0609c80
RodrigoLS <rolima13@gmail.com>
2017-09-27 14:05:40 -0300

    Add files via upload

---
+++ Rodrigo-Login/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>HIPPO</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+
+<section>
+	<div id="superior1">
+		<div id="superior2"> </div>
+	</div>
+
+	<div id="form">
+		<form id="frmLogin" method="POST">
+			<label>Usuário</label> <br>
+			<input type="text" name="usuario" id="usuario" maxlength="20">
+			<br><br>
+
+			<label>Senha</label> <br>
+			<input type="password" name="senha" id="senha" maxlength="20">
+			<br><br>
+		</form>
+	</div>
+</section>
+
+	<img src="img/Logo.jpg" id="Logo" width="" height="" alt="Logo da empresa, um hipopótamo juntamente com o nome da marca">
+
+<footer>
+	<p>Copyright© 2017</p>
+</footer>
+
+</body>
+</html>
