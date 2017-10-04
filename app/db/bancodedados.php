0d3d13380d227f011e7edb112298b04427851726
José Rodrigo Lima <rolima13@gmail.com>
2017-10-03 22:24:40 -0300

    Retornando a senha antiga do banco
    houve um engano.

--- app/db/bancodedados.php
+++ app/db/bancodedados.php
@@ -3,7 +3,7 @@
 $db_host = "pi-2-marcelo.database.windows.net";
 $db_name = "PI-Marcelo";
 $db_user = "marcelob133";
-$db_pass = "SenhaDoBanco";
+$db_pass = "!1InimaC1!";
 $dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
 
 if(!$db = odbc_connect($dsn, $db_user, $db_pass)){
