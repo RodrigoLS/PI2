49f7c823f6bd16d17a6c659a772d508dd34664de
José Rodrigo Lima <rolima13@gmail.com>
2017-10-07 21:32:35 -0300

    Troca de banco
    Remoção do meu banco utilizado nas aulas de PI e adicição do banco feito para o grupo.

--- app/db/bancodedados.php
+++ app/db/bancodedados.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "rodrigolima.database.windows.net";
-$db_name = "PI2";
-$db_user = "Rodrigo__Lima";
-$db_pass = "585119471998Jose*";
+$db_host = "bar100lona.database.windows.net";
+$db_name = "bar100lona";
+$db_user = "hippo";
+$db_pass = "Bar100lona";
 $dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
 
 if(!$db = odbc_connect($dsn, $db_user, $db_pass)){
