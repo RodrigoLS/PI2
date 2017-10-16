ef478415b26df3751bbdbaa28181384eab4a3b7e
MarceloBH <marceloborellihegedus@gmail.com>
2017-10-16 14:29:27 -0200

    Criação pasta  e alterações
    Criação pasta produto e alteração no cabeçalho do menu.

--- app/layout/cabecalho.php
+++ app/layout/cabecalho.php
@@ -3,8 +3,8 @@
 		<div class="nav-wrapper">
 			<ul  class="left">
 				<li><a href="../user">Usuario</a></li>
-				<li><a>Categoria</a></li>
-				<li><a>Produto</a></li>
+				<li><a href="../categoria">Categoria</a></li>
+				<li><a href="../produto">Produto</a></li>
 				<li><a href="sair.php">Sair</a></li>
 			</ul>
 		</div>
---
+++ app/produto/index.php
@@ -0,0 +1,66 @@
+<?php
+	include('../db/bancodedados.php');
+	include('../auth/controle.php');
+
+	//Funcionalidade Gravar Cadastro
+	if(isset($_POST['btnGravar'])){
+		unset($_GET['cadastrar']);
+		if(	!empty($_POST['loginUsuario']) && !empty($_POST['nomeUsuario']) && !empty($_POST['senhaUsuario'])){
+			$_POST['usuarioAtivo'] = isset($_POST['usuarioAtivo']) ? true : false;
+		
+			$stmt = odbc_prepare($db, "	INSERT INTO Categoria
+											(nomeCategoria,
+											descCategoria)
+										VALUES
+											(?,?)");
+
+			if(odbc_execute($stmt, array(	$_POST['nomeCategoria'],
+											$_POST['descCategoria'],))){
+				$msg = 'Categoria gravada com sucesso!';			
+			} else{
+				$erro = 'Erro ao gravar a categoria.';
+			}								
+							
+		} else{
+		
+			$erro = 'O campo: Nome é obrigatório.';
+		
+		}
+	}
+//FIM Funcionalidade Gravar Cadastro
+
+//Funcionalidade Listar
+$q = odbc_exec($db, 'SELECT idCategoria, nomeCategoria, descCategoria
+					 FROM Categoria');
+
+while($r = odbc_fetch_array($q)){
+	
+	$categorias[$r['idCategoria']] = $r;
+	
+}
+//FIM Funcionalidade Listar
+
+if(isset($_GET['cadastrar'])){//FORM Cadastrar
+
+	include('template_cadastrar.php');
+	
+}elseif(isset($_GET['editar'])){//FORM Editar
+
+	if(is_numeric($_GET['editar'])){
+		$q = odbc_exec($db, "	SELECT 	idCategoria, nomeCategoria,
+										descCategoria
+								FROM Categoria 
+								WHERE idCategoria = {$_GET['editar']}");
+		$dados_categorias = odbc_fetch_array($q);
+	}else{
+		$erro = 'Código inválido';
+	}
+
+	include('template_editar.php');
+	
+}else{//FORM Listar
+
+	include('template.php');
+	
+}
+?>
