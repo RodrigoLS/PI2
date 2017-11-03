2942d381a31c2603248e3e665cbab1de0857dc76
Marcelo B H <marceloborellihegedus@gmail.com>
2017-11-03 13:31:18 -0200

    Ajuste no form de cadastro Categoria
    Ajuste no form de cadastro Categoria

--- app/categoria/template_cadastrar.php
+++ app/categoria/template_cadastrar.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 	  <link rel="stylesheet" type="text/css" href="../css/estilo.css">
 	<title></title>
@@ -14,9 +15,17 @@
 	<form method="POST">
 		<fieldset>
 		<legend><b>NOVA CATEGORIA</b></legend>
-		<label>Nome:</label> <input type="text" name="nomeCategoria"> <br>
 
-		<label>Descrição:</label> <input type="text" name="descCategoria"> <br>
+		<div class="input-field">	
+			<input id="nome-categoria" type="text" name="nomeCategoria" class="validade">
+			<label for="nome-categoria"> Nome: </label>
+			<br>
+		</div>
+
+        <div class="input-field">
+          <textarea id="textarea1" class="materialize-textarea" name="descCategoria"></textarea>
+          <label for="textarea1">Descrição:</label>
+        </div>
 
 		<input type="submit" value="Gravar" name="btnGravar" class="waves-effect waves-light btn-large">
 		</fieldset>
